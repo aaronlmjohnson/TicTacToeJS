e56a5becb2eaa59e81a1dffbf979e75405f56c7a
Aaron <ajonaaronajon@yahoo.com>
2021-11-06 00:06:10 -0500

    cleaned up board factory

--- assets/src/Board.js
+++ assets/src/Board.js
@@ -2,7 +2,7 @@
  export const board = (()=>{
     let _board = [["X", "O", ""], ["O", "X", ""], ["", "X", "O"]];
     
-    function _render(){
+    const _render = ()=>{
         const board = document.createElement("div");
         board.id = "board";
         _createRows(board);
@@ -10,7 +10,7 @@
         document.body.appendChild(board);
     };
 
-    function update() {
+    const update = () =>{
         _board.forEach((row, i) =>{
             row.forEach((col, j) =>{
                 const column = document.getElementsByClassName(`${i}-${j}`)[0];
@@ -18,71 +18,71 @@
                 column.innerText = col;
             });
         });
-    }
+    };
 
-    function _createRows (board){
+    const _createRows = (board)=>{
         for(let i = 0; i < _board.length; i++){
             let row = document.createElement("div");
             row.className = "row";
             _createColumns(row, i);
             board.appendChild(row);
         }
-    }
+    };
 
-    function _createColumns(row, i){
+    const _createColumns = (row, i)=>{
         for(let j = 0; j < _board[i].length; j++){
             let col = document.createElement("div");
             col.className = `col ${i}-${j}`
             col.innerText = _board[i][j];
             row.appendChild(col);
         }
-    }
+    };
     const getValues = ()=>{
         return _board;
     };
-    function setCol(row, col, value){
+    const setCol = (row, col, value)=>{
         _board[row][col] = value;
-    }
+    };
 
-    function getCol(row, col) {
+    const getCol= (row, col)=> {
         return _board[row][col];
-    }
+    };
 
-    function getCols(){
+    const getCols = ()=>{
         return Array.from(document.getElementsByClassName("col"));
-    }
+    };
 
-    function getColCoords(element){
+    const getColCoords = (element)=>{
         if(element.className == "row")
             return;
         const coordStr = element.classList[1];
 
         return {row: parseInt(coordStr[0]), col: parseInt(coordStr[2])};
-    }
+    };
 
     const isFull = ()=>{
         return !_board.flat().includes("");
-    } 
+    };
 
     const rowValuesMatch = (row)=>{
         console.log(row);
         return _board[row].every((cell)=> cell === _board[row][0] && cell !== "" );
-    }
+    };
 
     const colValuesMatch = (col)=>{
         const column = _board.map(row => row[col]);
         return column.every((cell)=> cell === column[0] && column[0] !== "");
-    }
+    };;
 
     const negDiagMatch = ()=>{
         const diag = _board.map((row, i) =>{ return row[i]});
         return diag.every(cell => cell == diag[0] && diag[0] !== "");
-    }
+    };
 
     const posDiagMatch = ()=> {
         const diag = _board.map((row, i) => {return row[Math.abs(-2 + i)]});
         return diag.every(cell => cell == diag[0] && diag[0] !== "");
-    }
+    };
     
     const getCellElement = (row, col)=>{
         return document.getElementsByClassName(`${row}-${col}`)[0];
